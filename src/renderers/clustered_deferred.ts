701d33179a1ee2eb3f372e53afd328f75cef4e7f
dezelyu <dezelyu@seas.upenn.edu>
2024-10-16 21:21:11 -0400

    DEFERRED FOUNDATION COMMIT!

--- src/renderers/clustered_deferred.ts
+++ src/renderers/clustered_deferred.ts
@@ -3,20 +3,219 @@ import * as shaders from '../shaders/shaders';
 import { Stage } from '../stage/stage';
 
 export class ClusteredDeferredRenderer extends renderer.Renderer {
-    // TODO-3: add layouts, pipelines, textures, etc. needed for Forward+ here
-    // you may need extra uniforms such as the camera view matrix and the canvas resolution
-
+    
+    // declare the bind group layout for the scene
+    scene_bind_group_layout: GPUBindGroupLayout;
+    
+    // declare the bind group for the scene
+    scene_bind_group: GPUBindGroup;
+    
+    // declare the depth texture
+    depth_texture: GPUTexture;
+    
+    // declare the depth texture view
+    depth_texture_view: GPUTextureView;
+    
+    // declare the render pipeline layout
+    render_pipeline_layout: GPUPipelineLayout;
+    
+    // declare the render pipeline vertex shader module
+    render_pipeline_vertex_shader_module: GPUShaderModule;
+    
+    // declare the render pipeline fragment shader module
+    render_pipeline_fragment_shader_module: GPUShaderModule;
+    
+    // declare the render pipeline
+    render_pipeline: GPURenderPipeline;
+    
     constructor(stage: Stage) {
         super(stage);
-
-        // TODO-3: initialize layouts, pipelines, textures, etc. needed for Forward+ here
-        // you'll need two pipelines: one for the G-buffer pass and one for the fullscreen pass
+        
+        // create the bind group layout for the scene
+        this.scene_bind_group_layout = renderer.device.createBindGroupLayout({
+            label: "scene_bind_group_layout",
+            entries: [
+                
+                // create a new bind group layout entry for the camera's uniform buffer
+                {
+                    // specify the binding index
+                    binding: 0,
+                    
+                    // specify the shader stage to be the vertex and fragment shader
+                    visibility: GPUShaderStage.VERTEX | GPUShaderStage.FRAGMENT,
+                    
+                    // specify the buffer type to be uniform
+                    buffer: {
+                        type: "uniform",
+                    },
+                },
+            ],
+        });
+        
+        // create the bind group for the scene
+        this.scene_bind_group = renderer.device.createBindGroup({
+            label: "scene_bind_group",
+            layout: this.scene_bind_group_layout,
+            entries: [
+                
+                // create a new bind group entry for the camera's uniform buffer
+                {
+                    // specify the binding index
+                    binding: 0,
+                    
+                    // specify the resource to be the camera's uniform buffer
+                    resource: {
+                        buffer: this.camera.uniformsBuffer,
+                    },
+                },
+            ],
+        });
+        
+        // create the depth texture
+        this.depth_texture = renderer.device.createTexture({
+            label: "depth_texture",
+            size: [
+                renderer.canvas.width,
+                renderer.canvas.height,
+            ],
+            format: "depth24plus",
+            usage: GPUTextureUsage.RENDER_ATTACHMENT,
+        });
+        
+        // create the depth texture view
+        this.depth_texture_view = this.depth_texture.createView();
+        
+        // create the render pipeline layout
+        this.render_pipeline_layout = renderer.device.createPipelineLayout({
+            label: "render_pipeline_layout",
+            bindGroupLayouts: [
+                this.scene_bind_group_layout,
+                renderer.modelBindGroupLayout,
+                renderer.materialBindGroupLayout,
+            ],
+        });
+        
+        // declare the render pipeline vertex shader module
+        this.render_pipeline_vertex_shader_module = renderer.device.createShaderModule({
+            label: "render_pipeline_vertex_shader_module",
+            code: shaders.naiveVertSrc,
+        });
+        
+        // declare the render pipeline fragment shader module
+        this.render_pipeline_fragment_shader_module = renderer.device.createShaderModule({
+            label: "render_pipeline_fragment_shader_module",
+            code: shaders.clusteredDeferredFragSrc,
+        });
+        
+        // create the render pipeline
+        this.render_pipeline = renderer.device.createRenderPipeline({
+            label: "render_pipeline",
+            layout: this.render_pipeline_layout,
+            depthStencil: {
+                depthWriteEnabled: true,
+                depthCompare: "less",
+                format: "depth24plus",
+            },
+            vertex: {
+                module: this.render_pipeline_vertex_shader_module,
+                buffers: [
+                    renderer.vertexBufferLayout,
+                ],
+            },
+            fragment: {
+                module: this.render_pipeline_fragment_shader_module,
+                targets: [
+                    {
+                        format: renderer.canvasFormat,
+                    },
+                ],
+            },
+        });
     }
 
     override draw() {
-        // TODO-3: run the Forward+ rendering pass:
-        // - run the clustering compute shader
-        // - run the G-buffer pass, outputting position, albedo, and normals
-        // - run the fullscreen pass, which reads from the G-buffer and performs lighting calculations
+        
+        // create a new command encoder
+        const encoder = renderer.device.createCommandEncoder();
+        
+        // execute the compute shader
+        this.lights.compute(encoder);
+        
+        // create the attachment view where the scene is rendered
+        const attachment_view = renderer.context.getCurrentTexture().createView();
+        
+        // create and start the render pass
+        const render_pass = encoder.beginRenderPass({
+            label: "render_pass",
+            colorAttachments: [
+                {
+                    view: attachment_view,
+                    clearValue: [
+                        0,
+                        0,
+                        0,
+                        0,
+                    ],
+                    loadOp: "clear",
+                    storeOp: "store",
+                },
+            ],
+            depthStencilAttachment: {
+                view: this.depth_texture_view,
+                depthClearValue: 1.0,
+                depthLoadOp: "clear",
+                depthStoreOp: "store",
+            },
+        });
+        
+        // bind the render pipeline
+        render_pass.setPipeline(this.render_pipeline);
+        
+        // bind the scene's bind group to the render pass
+        render_pass.setBindGroup(
+            shaders.constants.bindGroup_scene,
+            this.scene_bind_group
+        );
+        
+        // bind additional shader resources and render
+        this.scene.iterate(node => {
+            
+            // bind the model matrices
+            render_pass.setBindGroup(
+                shaders.constants.bindGroup_model,
+                node.modelBindGroup
+            );
+        }, material => {
+            
+            // bind the textures and samplers
+            render_pass.setBindGroup(
+                shaders.constants.bindGroup_material,
+                material.materialBindGroup
+            );
+        }, primitive => {
+            
+            // bind the vertex buffer
+            render_pass.setVertexBuffer(
+                0, primitive.vertexBuffer
+            );
+            
+            // bind the index buffer
+            render_pass.setIndexBuffer(
+                primitive.indexBuffer, "uint32"
+            );
+            
+            // render the primitive
+            render_pass.drawIndexed(
+                primitive.numIndices
+            );
+        });
+        
+        // end the render pass
+        render_pass.end();
+        
+        // submit the encoded commands
+        renderer.device.queue.submit([
+            encoder.finish(),
+        ]);
     }
 }
--- src/shaders/clustered_deferred.fs.wgsl
+++ src/shaders/clustered_deferred.fs.wgsl
@@ -1,3 +1,44 @@
-// TODO-3: implement the Clustered Deferred G-buffer fragment shader
 
-// This shader should only store G-buffer information and should not do any shading.
+// declare the variable for the camera
+@group(${bindGroup_scene}) @binding(0)
+var<uniform> camera: CameraUniforms;
+
+// declare the diffuse texture
+@group(${bindGroup_material}) @binding(0)
+var diffuse_texture: texture_2d<f32>;
+
+// declare the diffuse texture sampler
+@group(${bindGroup_material}) @binding(1)
+var diffuse_texture_sampler: sampler;
+
+// declare the intermediate data
+struct IntermediateData {
+    
+    // declare the fragment position
+    @location(0) point: vec3f,
+    
+    // declare the fragment normal
+    @location(1) normal: vec3f,
+    
+    // declare the fragment texture coordinate
+    @location(2) coordinate: vec2f
+};
+
+// declare the fragment shader
+@fragment fn main(data: IntermediateData) -> @location(0) vec4f {
+    
+    // acquire the diffuse color
+    var color = textureSample(
+        diffuse_texture,
+        diffuse_texture_sampler,
+        data.coordinate
+    );
+    
+    // perform alpha cut with a threshold of 0.5
+    if (color.a < 0.5f) {
+        discard;
+    }
+    
+    // compute and return the fragment color
+    return vec4(color.rgb, 1.0f);
+}
